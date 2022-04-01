db47053bfb9a001e6c52e8e48c7bccabc9d368b3
Lexy Joseph <lexy.elezebeth@gmail.com>
2022-04-01 22:59:12 +0530

    HomeComponent added

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,34 +1,49 @@
-import React from "react";
-import { Card, CardImg, CardTitle, CardBody } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 
-function RenderHighlights ({item}) {
-    return (
-        <Card>
-            <CardImg src={item.image} alt={item.name} />
-            <CardTitle>{item.name}</CardTitle>
-            <CardBody>
-                {item.description}
-            </CardBody>
-        </Card>
-    );
-}
-
-function Home(props) {
+const Home = (props) => {
     return(
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                    <RenderHighlights item={props.dish} />
+                    <Card>
+                        <CardImg src="/assets/images/burger.jpg" alt="Burger"/>
+                        <CardBody>
+                            <CardTitle>Mexican Burger</CardTitle>
+                            <CardSubtitle>The iconic one</CardSubtitle>
+                            <CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</CardText>
+                            <Button>See details</Button>
+                        </CardBody>
+                    </Card>
                 </div>
+
                 <div className="col-12 col-md m-1">
-                    <RenderHighlights item={props.event} />
+                    <Card>
+                        <CardImg src="/assets/images/blog1.jpg" alt="Burger"/>
+                        <CardBody>
+                            <CardTitle>Weekend Buffet</CardTitle>
+                            <CardSubtitle>Enjoy our weekend fest</CardSubtitle>
+                            <CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</CardText>
+                            <Button>Know more</Button>
+                        </CardBody>
+                    </Card>
                 </div>
+
                 <div className="col-12 col-md m-1">
-                    <RenderHighlights item={props.blog} />
+                    <Card>
+                        <CardImg src="/assets/images/blog1.jpg" alt="Burger"/>
+                        <CardBody>
+                            <CardTitle>Blog</CardTitle>
+                            <CardSubtitle>Read our Blog</CardSubtitle>
+                            <CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</CardText>
+                            <Button>Read More</Button>
+                        </CardBody>
+                    </Card>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,40 +1,17 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { EVENTS } from '../shared/events';
-import { DISHES } from '../shared/dishes';
-import { BLOGS } from '../shared/blogs';
-import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './HomeComponent';
 
-class Main extends Component {
-    constructor (props) {
-        super (props);
 
-        this.state = {
-            dishes: DISHES,
-            events: EVENTS,
-            blogs: BLOGS
-        };
-    }
+class Main extends Component {
+    
     render() {
         
-        const HomePage =() => {
-            return(
-                <Home>
-                    dish = {this.state.dishes.filter((dish) => dish.featured)[0]}
-                    event = {this.state.events.filter((even) => even.featured)[0]}
-                    blog = {this.state.blogs.filter((blo) => blo.featured)[0]}
-                </Home>
-            )
-        }
-
         return (
             <div>
                 <Header />
-                <Routes>
-                    <Route path="/home" element={HomePage} />
-                </Routes>
+                <Home />
                 <Footer /> 
             </div>
         );
