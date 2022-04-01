31fd7afc5fe0312876a6213d26969a4aae3696b3
Lexy Joseph <lexy.elezebeth@gmail.com>
2022-04-01 17:49:22 +0530

    Reserve Form added with validation

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -4,6 +4,8 @@ import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
     Row, Col, Label, NavLink} from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
+
+
 //For Form Validation:
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -19,13 +21,18 @@ class Header extends Component {
         this.state={
             isNavOpen: false,
             isLoginModalOpen: false,
-            isSignupModalOpen: false
+            isSignupModalOpen: false,
+            isBookingModalOpen: false
         }
         this.toggleNav = this.toggleNav.bind(this);
         this.toggleLoginModal = this.toggleLoginModal.bind(this);
         this.toggleSignupModal = this.toggleSignupModal.bind(this);
+        this.toggleBookingModal = this.toggleBookingModal.bind(this);
         this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
-        this.handleSignupSubmit = this.handleSignupSubmit.bind(this);   
+        this.handleSignupSubmit = this.handleSignupSubmit.bind(this);
+        this.handleBookingFormSubmit = this.handleBookingFormSubmit.bind(this);
+
+   
     }
 
     toggleNav() {
@@ -46,6 +53,12 @@ class Header extends Component {
         });
     }
 
+    toggleBookingModal() {
+        this.setState({
+            isBookingModalOpen: !this.state.isBookingModalOpen
+        });
+    }
+
 
     handleLoginSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
@@ -57,7 +70,14 @@ class Header extends Component {
         alert('Current State is: ' + JSON.stringify(values));
     }
 
+    handleBookingFormSubmit(values){
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+     
+    }
+
     render() {
+        
         return(
             <React.Fragment>
                 <Navbar dark expand="md">
@@ -112,8 +132,8 @@ class Header extends Component {
                             <div className="col-12 col-sm-5">
                                 <h1>DELICIOUS DINER</h1> <br/>
                                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat".</p>
-                                <Button>
-                                    Book a Table 
+                                <Button outline onClick={this.toggleBookingModal}>
+                                    Reserve Table 
                                 </Button>
                             </div>
                         </div>
@@ -304,6 +324,144 @@ class Header extends Component {
                         </LocalForm>
                     </ModalBody>
                 </Modal>
+
+                {/* Booking Table MODAL: */}
+                <Modal isOpen={this.state.isBookingModalOpen}
+                toggle={this.toggleBookingModal}>
+                    <ModalHeader toggle={this.toggleBookingModal}>
+                        Reservation Form:
+                    </ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleBookingFormSubmit(values)} >
+                        <Row className="form-group">
+                        <Label htmlFor="fullname" md={12}>Enter your Full Name:</Label>
+                        <Col md={12}>
+                            <Control.text model=".fullname" id="fullname" name="fullname"
+                            placeholder="Full Name"
+                            className="form-control"
+                            validators={{
+                                required, minLength: minLength(3), maxLength:maxLength(15)
+                            }}>
+                            </Control.text>
+                            <Errors
+                                className="text-danger"
+                                model=".fullname"
+                                show="touched"
+                                messages={{
+                                    required: 'Required ',
+                                    minLength:'Must be greater than 2 characters',
+                                    maxLength: 'Must be 15 characters or less'
+                                }}
+                            />
+                        </Col>
+                        </Row>
+
+                        <Row className="form-group">
+                            <Label htmlFor="guestsnumber" md={12}>Number of Guests:</Label>
+
+                                    <Col md={12}>
+                                        <Control.select model=".guestsnumber" name="guestsnumber" id="guestsnumber"
+                                            className="form-control"
+                                            validators={{
+                                                required
+                                            }}>
+                                            <option>Select</option>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                            <option>6</option>
+                                        </Control.select>
+                                        <Errors
+                                            className="text-danger"
+                                            model=".guestsnumber"
+                                            show="touched"
+                                            messages={{
+                                                required: 'Required ',                                            
+                                            }} >
+                                        </Errors>
+                                    </Col>
+                        </Row>
+
+                        <Row className="form-group">
+                                <Label htmlFor="date" md={12}>Select the date(dd-mm-yyyy):</Label>
+                                <Col md={12}>
+                                    <Control.text model=".date" id="date" name="date"
+                                        placeholder="Enter the date"
+                                        className="form-control"
+                                        validators={{
+                                            required
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".date"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required '
+                                        }}
+                                     />
+                                </Col>
+                        </Row>
+                        
+                        <Row className="form-group">
+                                <Label htmlFor="telnum" md={12}>Contact Number:</Label>
+                                <Col md={12}>
+                                    <Control.text model=".telnum" id="telnum" name="telnum"
+                                        placeholder="Tel. Number"
+                                        className="form-control"
+                                        validators={{
+                                            required, isNumber
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".telnum"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required ',
+                                            isNumber: 'Must be a number'
+                                        }}
+                                     />
+                                </Col>
+                        </Row>
+
+                        <Row className="form-group">
+                        <Label htmlFor="email" md={12}>Email:</Label>
+                        <Col md={12}>
+                            <Control.text model=".email" id="email" name="email"
+                            placeholder="E-mail"
+                            className="form-control"
+                            validators={{
+                                required, validEmail
+                            }} >
+                            </Control.text>
+                            <Errors
+                                className="text-danger"
+                                model=".email"
+                                show="touched"
+                                messages={{
+                                    required: 'Required ',
+                                    validEmail: 'Invalid Email Address'
+                                }}
+                            /> 
+                        </Col>
+                        </Row>
+
+                        <Row className="form-group">
+                                <Col md={{size:10}}>
+                                    <Button type="submit" color="primary">
+                                    Reserve
+                                    </Button>
+                                </Col>
+                            </Row>
+                    
+
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+                
 
             </React.Fragment>
         );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,8 +6,7 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <Header />
-                
+                <Header />    
             </div>
         );
     }
